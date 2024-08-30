f09f3ed8e8bec5f026098e83584b72072b2f3bf1
star-dev <starfatestarfate@gmail.com>
2024-08-30 08:08:37 +0200

    fix: format date issue

--- components/dashboard/ItemsListView/index.vue
+++ components/dashboard/ItemsListView/index.vue
@@ -4,9 +4,6 @@
       <h1 class="text-2xl font-bold">Items List</h1>
       <CommonButton label="Add new item" @click="openNewDialog"></CommonButton>
     </div>
-    <div class="flex justify-center items-center min-h-20">
-      <div>No data</div>
-    </div>
     <table v-if="items?.length" class="w-full">
       <thead>
         <tr>
@@ -18,17 +15,22 @@
       <tbody>
         <tr v-for="item in items" :key="item.id">
           <td class="py-2 px-4 border-b border-gray-200">{{item.name}}</td>
-          <td class="py-2 px-4 border-b border-gray-200">{{item.date}}</td>
+          <td class="py-2 px-4 border-b border-gray-200">{{formatDate(item.date)}}</td>
           <td class="py-2 px-4 border-b border-gray-200">{{item.important ? 'Yes' : 'No'}}</td>
         </tr>
       </tbody>
     </table>
+    <div v-else class="flex justify-center items-center min-h-20">
+      <div>No data</div>
+    </div>
 
     <DashboardItemCreateDialog :is-open="isOpen" @close="onCloseDialog"></DashboardItemCreateDialog>
   </div>
 </template>
 
 <script setup lang="ts">
+import dayjs from 'dayjs';
+
 const { getItems } = useDirectusItems();
 
 const { data: items, refresh } = await useAsyncData<any>('items', () => {
@@ -54,5 +56,9 @@ const onCloseDialog = (value: boolean) => {
   if (value) {
     refresh();
   }
+}
+
+const formatDate = (date: Date) => {
+  return dayjs(date).format('DD/MM/YYYY')
 }
 </script>
